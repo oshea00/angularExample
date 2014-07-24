6c6c374414cd918a01040146463b8fdb3b3e2130
Mike OShea <mike@limpidfox.com>
2014-07-24 16:59:16 -0700

    make dialog a service

--- public/javascripts/app/accountController.js
+++ public/javascripts/app/accountController.js
@@ -1,49 +1,4 @@
-﻿app.filter('match', function () {
-    return function (input, lookup, fields) {
-        var field = fields.split(",");
-        var match = _.find(lookup, function (item) {
-            return item[field[0]] === input;
-        });
-        if (match != undefined)
-            return match[field[1]];
-        else
-            return input;
-    }
-});
-
-app.directive('isNumber', ['numberFilter',function(numberFilter){
-    return{
-        require:'ngModel',
-        link: function(scope, elem, attrs, ctrl){
-            var digits = attrs.isNumber.length===0 ? "2" : attrs.isNumber;
-            ctrl.$parsers.unshift(checkForNumber);
-            ctrl.$formatters.unshift(formatNumber);
-
-            function checkForNumber(viewValue){
-
-                // Checks for positive or negative decimal or integer number with or without thousand separators
-                if (/^-{0,1}\d{1,3}(,\d{3})*\.{0,1}\d*$|^-{0,1}\d*\.{0,1}\d*$/.test(viewValue)) {
-                    ctrl.$setValidity('isNumber',true);
-                }
-                else{
-                    ctrl.$setValidity('isNumber', false);
-                }
-                return viewValue.replace(/,/g,'');
-            }
-
-            function formatNumber(viewValue) {
-                return numberFilter(viewValue,digits);
-            }
-        }
-    };
-}]);
-
-function convertToNumber(value)
-{
-    return (typeof value === 'string') ? Number(value.replace(/,/g,'')) : value;
-}
-
-app.controller('AccountController', function ($scope, $log, $modal, $window, Restangular) {
+﻿app.controller('AccountController', function ($scope, $log, $modal, $window, Restangular, accountDialog) {
 
     $scope.selections = [];
     $scope.loaded = false;
@@ -163,26 +118,7 @@ app.controller('AccountController', function ($scope, $log, $modal, $window, Res
             angular.copy($scope.selections[0], acct);
         }
 
-        var modalInstance = $modal.open({
-            templateUrl: 'accountDlg.html',
-            controller: AccountDlgCtrl,
-            size: size,
-            backdrop: 'static',
-            resolve: {
-                accounts: function () {
-                    return $scope.accounts;
-                },
-                accountTypes: function () {
-                    return $scope.accountTypes;
-                },
-                account: function () {
-                    return acct;
-                },
-                doThis: function () {
-                    return doThis;
-                }
-            }
-        });
+        var modalInstance = accountDialog.open(size,acct,doThis,$scope.accounts,$scope.accountTypes);
 
         modalInstance.result.then(function (result) {
             if (result.doThis === 'Delete')
@@ -196,36 +132,3 @@ app.controller('AccountController', function ($scope, $log, $modal, $window, Res
 
 });
 
-var AccountDlgCtrl = function ($scope, $modalInstance, accounts, accountTypes, account, doThis) {
-
-    $scope.accounts = accounts;
-    $scope.accountTypes = accountTypes;
-    $scope.account = account;
-    $scope.doThis = doThis;
-    $scope.unique = true;
-    $scope.delete = false;
-
-    if (doThis === 'Delete')
-        $scope.delete = true;
-
-    $scope.uniqueAccountCode = function () {
-        var found = false;
-
-        found = _.find(accounts, function (a) {
-            if (a._id != $scope.account._id && a.AccountCode === $scope.account.AccountCode)
-                return true;
-            else
-                return false;
-        });
-        $scope.unique = !found;
-        return !found;
-    };
-
-    $scope.ok = function () {
-        $modalInstance.close({ account: $scope.account, doThis: $scope.doThis });
-    };
-
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
-};
---
+++ public/javascripts/app/appGlobals.js
@@ -0,0 +1,105 @@
+app.filter('match', function () {
+    return function (input, lookup, fields) {
+        var field = fields.split(",");
+        var match = _.find(lookup, function (item) {
+            return item[field[0]] === input;
+        });
+        if (match != undefined)
+            return match[field[1]];
+        else
+            return input;
+    }
+});
+
+app.directive('isNumber', ['numberFilter',function(numberFilter){
+    return{
+        require:'ngModel',
+        link: function(scope, elem, attrs, ctrl){
+            var digits = attrs.isNumber.length===0 ? "2" : attrs.isNumber;
+            ctrl.$parsers.unshift(checkForNumber);
+            ctrl.$formatters.unshift(formatNumber);
+
+            function checkForNumber(viewValue){
+
+                // Checks for positive or negative decimal or integer number with or without thousand separators
+                if (/^-{0,1}\d{1,3}(,\d{3})*\.{0,1}\d*$|^-{0,1}\d*\.{0,1}\d*$/.test(viewValue)) {
+                    ctrl.$setValidity('isNumber',true);
+                }
+                else{
+                    ctrl.$setValidity('isNumber', false);
+                }
+                return viewValue.replace(/,/g,'');
+            }
+
+            function formatNumber(viewValue) {
+                return numberFilter(viewValue,digits);
+            }
+        }
+    };
+}]);
+
+app.factory('accountDialog', ['$modal',function($modal){
+
+    function open(size,acct,doThis,accounts,accountTypes) {
+        return $modal.open({
+            templateUrl: 'accountDlg.html',
+            controller: AccountDlgCtrl,
+            size: size,
+            backdrop: 'static',
+            resolve: {
+                accounts: function () {
+                    return accounts;
+                },
+                accountTypes: function () {
+                    return accountTypes;
+                },
+                account: function () {
+                    return acct;
+                },
+                doThis: function () {
+                    return doThis;
+                }
+            }
+        });
+    }
+
+    return {
+        open : open
+    };
+
+}]);
+
+var AccountDlgCtrl = function ($scope, $modalInstance, accounts, accountTypes, account, doThis) {
+
+    $scope.accounts = accounts;
+    $scope.accountTypes = accountTypes;
+    $scope.account = account;
+    $scope.doThis = doThis;
+    $scope.unique = true;
+    $scope.delete = false;
+
+    if (doThis === 'Delete')
+        $scope.delete = true;
+
+    $scope.uniqueAccountCode = function () {
+        var found = false;
+
+        found = _.find(accounts, function (a) {
+            if (a._id != $scope.account._id && a.AccountCode === $scope.account.AccountCode)
+                return true;
+            else
+                return false;
+        });
+        $scope.unique = !found;
+        return !found;
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close({ account: $scope.account, doThis: $scope.doThis });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
+
